e61ec191cd87fc2e551aa3b2831ee2d7896eccd3
Jimmy Cai <hi@jimmycai.com>
2023-04-04 10:18:08 +0000

    Que lance la excepcion en Parser/Test.java, para que sea facil de debuggear

--- ditto/parser/Test.java
+++ ditto/parser/Test.java
@@ -1,23 +1,16 @@
 package ditto.parser;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 import ditto.lexer.Lexer;
 
 public class Test {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws Exception {
         Reader input = new InputStreamReader(new FileInputStream(args[0]));
         Lexer lexer = new Lexer(input);
         Parser parser = new Parser(lexer);
-        try{
-            parser.parse();
-        } catch (Exception e) {
-            System.out.println("Palurdo");
-            System.out.println(e.getMessage());
-        }
+        parser.parse();
     }
 }

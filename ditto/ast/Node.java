9af90951318eb2ff4a1004613b6b4f6b0f139a55
ayhon <43295942+ayhon@users.noreply.github.com>
2023-04-17 20:08:31 +0200

    Ideas de última parte de la práctica
    Co-authored-by: Jimmy Cai <hi@jimmycai.com>
    Co-authored-by: ErVinuelas <ErVinuelas@users.noreply.github.com>

--- ditto/ast/Node.java
+++ ditto/ast/Node.java
@@ -4,11 +4,40 @@ import java.util.List;
 import java.util.StringJoiner;
 
 public abstract class Node {
-    // public ?? type() // for the future
-    // public ?? bind() // for the future
-    // public ?? generateCode() // for the future
+    /*
+    type LocalScopes = Map<String, (DefVar | DefFunc | DefStruct | DefModule)>;
+    type Context = List<LocalScopes>;
+    class Context {
+        GlobalScope globalScope,
+        List<LocalScopes> localScopes;
+        List<List<LocalScopes>> otros;
+        public getDef(String id);
+        public enterFunction();
+    } 
+    void bind(gl, scs){
+        for (Statement inst : this.body)
+            inst.bind(gl, new ArrayList<>());
+    }
+    */
+    // type GlobalScope = Map<String, DefVar>;
+    public abstract void bind(GlobalScope globalScope, List<LocalScopes> localScopes) {
+        for(Node child : getAstArguments()){
+            child.bind();
+        }
+    }
+    // Vincular: uso de variables (Var), tanto simpes identificadores como modulo::iden, con definición (DefVar)
+    //           uso de funciones (Call) con definición (DefFunc)
+    //           uso de estructuras (StructLiteral) con definición (DefStruct)
+    //           use de módulos (DefModule) con definición (Module) que tendríamos que parsear
+    
+    public abstract Type type(); // Desde Module se llama antes a `bind`. El árbol debe estar vinculado paque se pueda tipar
+    // Te devuelve el tipo de expreisiones
+    // si se aplica a un nodo que no es una expresión, devuelve Null.getInstance()
+    
+    public abstract void generateCode(ProgramOutput out); // Desde Module llama a `type` antes de recursar,
+    // type ProgramOutput = StringBuilder;
+    
     public abstract String getAstString();
-
     public abstract List<Object> getAstArguments();
 
     @Override

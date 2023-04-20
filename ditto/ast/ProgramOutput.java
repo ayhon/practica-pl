5a8ee4bfaf1f97666841ab648e471ae511ee5deb
Jimmy Cai <hi@jimmycai.com>
2023-04-20 18:10:26 +0000

    Definir ProgramOutput

--- ditto/ast/ProgramOutput.java
+++ ditto/ast/ProgramOutput.java
@@ -1,6 +1,18 @@
 package ditto.ast;
 
-public class ProgramOutput  {
+public class ProgramOutput {
+    /// Clase que va encadenando las instrucciones de salida
+    private final StringBuilder sb = new StringBuilder();
+
     public ProgramOutput() {
+    }
+
+    public void append(String s) {
+        sb.append(s);
+    }
+
+    @Override
+    public String toString() {
+        return this.sb.toString();
     }
 }

bfde2a4758876a1589c7456878b5a412ff76286b
ayhon <43295942+ayhon@users.noreply.github.com>
2023-04-24 10:13:58 +0200

    `DefinitionCollection` clase interna de `Module`

--- ditto/ast/Module.java
+++ ditto/ast/Module.java
@@ -35,6 +35,47 @@ public class Module extends Node {
         this.structs = definitions.getStructs();
         this.globals = definitions.getVariables();
     }
+    public class DefinitionCollection {
+        private List<DefVar> variables;
+    
+        public List<DefVar> getVariables() {
+            return variables;
+        }
+    
+        private List<DefFunc> functions;
+    
+        public List<DefFunc> getFunctions() {
+            return functions;
+        }
+    
+        private List<DefStruct> structs;
+    
+        public List<DefStruct> getStructs() {
+            return structs;
+        }
+    
+        public void setStructs(List<DefStruct> structs) {
+            this.structs = structs;
+        }
+    
+        public DefinitionCollection() {
+            this.variables = new ArrayList<>();
+            this.functions = new ArrayList<>();
+            this.structs = new ArrayList<>();
+        }
+    
+        public void add(DefVar var) {
+            this.variables.add(var);
+        }
+    
+        public void add(DefFunc func) {
+            this.functions.add(func);
+        }
+    
+        public void add(DefStruct struct) {
+            this.structs.add(struct);
+        }
+    }
 
     @Override
     public String getAstString() { return "module"; }

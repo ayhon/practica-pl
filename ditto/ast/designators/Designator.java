1b36e1f35f18dc84d9528711fcf6912aff4b1cd6
ayhon <43295942+ayhon@users.noreply.github.com>
2023-04-25 19:36:11 +0200

    Añadir métodos de compilación específicos

--- ditto/ast/designators/Designator.java
+++ ditto/ast/designators/Designator.java
@@ -1,6 +1,14 @@
 package ditto.ast.designators;
 
+import ditto.ast.ProgramOutput;
 import ditto.ast.expressions.Expr;
 
 public abstract class Designator extends Expr {
+    public abstract void compileAsDesig(ProgramOutput out);
+
+    @Override
+    public void compileAsExpr(ProgramOutput out) {
+        compileAsDesig(out);
+        out.i32_load();
+    }
 }
--- ditto/ast/expressions/Expr.java
+++ ditto/ast/expressions/Expr.java
@@ -1,7 +1,15 @@
 package ditto.ast.expressions;
 
+import ditto.ast.ProgramOutput;
 import ditto.ast.statements.Statement;
 
 public abstract class Expr extends Statement {
     /// Todas las expresiones tienen que tener un tipo
+    public abstract void compileAsExpr(ProgramOutput out);
+
+    @Override
+    public void compile(ProgramOutput out) {
+        compileAsExpr(out);
+        out.drop();
+    }
 }

d22afdd7665dc170d7057ab1c994c808e66214d9
ayhon <43295942+ayhon@users.noreply.github.com>
2023-05-01 01:16:57 +0200

    `run.sh test ... all` no para tras el primer fallo

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 JAR_PATH="./lib";
 TASKS=(
-    "ast"
-    "bind"
-    "typesize"
-    "typecheck"
-    "offsets"
+    "ast" 
+    "bind" 
+    "typecheck" 
+    "typesize" 
+    "offsets" 
     "code"
 );
 
@@ -59,14 +59,13 @@ case $1 in
             all)
                 # Y el resto de veces ejecuto los tests sin compilar
                 export DO_NOT_COMPILE=1
-
                 # Ejecutar todos los casos de prueba que hay en test/pass/*.ditto
                 # Mediante un for
                 for test_file in test/pass/*.ditto; do
                     ditto_test $task $test_file $print_ast
                     if [ $? -ne 0 ]; then
                         echo "❌ failed $test_file - $task"
-                        exit 1
+                        failed="yes"
                     else
                         echo "✅ passed $test_file - $task"
                     fi
@@ -76,37 +75,41 @@ case $1 in
                 # Hay que ejecutar todas las tareas que van antes de $task, y deberian de pasar sin problema
                 # Y cuando llega a la tarea $task deberia de fallar, y terminar
                 # Iterar por fichero fuera, y dentro por tareas
-                echo "🧪 Ejecutando los tests que deberian fallar"
+                echo "Ejecutando los tests que deberian fallar"
                 for test_file in test/fail/$task/*.ditto; do
-                    echo "  📁 Ejecutando $test_file"
+                    echo "Ejecutando $test_file"
                     for task_i in "${TASKS[@]}"; do
+                        echo "Ejecutando $task_i - $test_file"
                         ditto_test $task_i $test_file $print_ast
                         RESULT=$?
                         # Deberia fallar la ejecucion si la tarea es $task
                         # Si no falla, lanza un mensaje de error
                         if [ $task_i == $task ]; then
                             if [ $RESULT -eq 0 ]; then
-                                echo "      ❌ passed $test_file - $task_i y NO deberia"
-                                exit 1
+                                echo "❌ passed $test_file - $task_i y NO deberia"
+                                failed="yes"
                             else 
-                                echo "      ✅ failed $test_file - $task_i y deberia"
+                                echo "✅ failed $test_file - $task_i y deberia"
                             fi
                         # si no, deberia pasar
                         else
                             if [ $RESULT -ne 0 ]; then
-                                echo "      ❌ failed $test_file - $task_i y NO deberia"
-                                exit 1
+                                echo "❌ failed $test_file - $task_i y NO deberia"
+                                failed="yes"
                             else
-                                echo "      ✅ passed $test_file - $task_i y deberia"
+                                echo "✅ passed $test_file - $task_i y deberia"
                             fi
                         fi
 
                         # Pasar al siguiente fichero cuando se ha ejecutado la tarea $task
-                        if [ $task_i == $task ]; then
+                        if [ $task == $task ]; then
                             break
                         fi
                     done
                 done
+                if [ "$failed" == "yes" ]; then
+                    exit 1
+                fi
                 ;;
             *)
                 ditto_test $task $test_file $print_ast

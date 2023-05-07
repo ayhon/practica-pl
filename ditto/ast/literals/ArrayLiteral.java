c169895e98df071a4388e6ae5f4455fd7b2546b2
Jimmy <hi@jimmycai.com>
2023-05-07 09:08:14 +0200

    Crear la funcion swap en WASM para evitar usar memoria auxiliar en ArrayLiteral

--- ditto/ast/literals/ArrayLiteral.java
+++ ditto/ast/literals/ArrayLiteral.java
@@ -119,27 +119,25 @@ public class ArrayLiteral extends Literal {
         out.i32_const(this.type().size());
         out.call(ProgramOutput.RESERVE_HEAP);
 
-        /// Guardar dirección del inicio del array
-        out.mem_local(this.position);
+        /// Guardar dirección del inicio del array al final de pila, para devolver
+        /// despues
         out.get_global("NP");
-        out.i32_store();
 
         out.indented(() -> {
             /// Evaluar cada elemento (puede contener también tipos no básicos)
             for (int i = 0; i < elements.size(); ++i) {
+                out.comment("Preparar la dirección destino");
+                out.duplicate();
+                out.i32_const(i * elements.get(i).type().size());
+                out.i32_add();
+
                 var element = elements.get(i);
                 out.comment("Guardando elemento " + i + " del array: " + element.decompile());
 
                 if (element.type().isBasic) {
                     /// Caso base, copiar con i32_store
                     out.comment("Es un tipo básico, copiar con i32_store");
-                    out.mem_local(this.position);
-                    out.i32_load();
-                    out.i32_const(i * elements.get(i).type().size());
-                    out.i32_add();
-
                     element.compileAsExpr(out);
-
                     out.i32_store();
                 } else {
                     /// Caso recursivo, copiar con ncopy
@@ -151,10 +149,8 @@ public class ArrayLiteral extends Literal {
                     });
 
                     out.comment("TO");
-                    out.mem_local(this.position);
-                    out.i32_load();
-                    out.i32_const(i * elements.get(i).type().size());
-                    out.i32_add();
+                    out.comment("Intercambio el cima de pila, porque la dirección destino ya estaba calculada antes");
+                    out.call(ProgramOutput.SWAP);
 
                     out.comment("SIZE");
                     out.i32_const(element.type().size() / 4);
@@ -163,9 +159,5 @@ public class ArrayLiteral extends Literal {
                 }
             }
         });
-
-        /// Devolver dirección del inicio del array
-        out.mem_local(this.position);
-        out.i32_load();
     }
 }

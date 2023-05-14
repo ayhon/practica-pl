6c29aa2bb82837f4d1202040a4baea5b5132287e
ayhon <43295942+ayhon@users.noreply.github.com>
2023-05-15 01:55:25 +0200

    Evitar la creación del AST en presencia de errores

--- ditto/errors/ErrorHandler.java
+++ ditto/errors/ErrorHandler.java
@@ -3,8 +3,14 @@ package ditto.errors;
 import ditto.lexer.Token;
 
 public class ErrorHandler {
+    private int errors = 0;
+    public boolean hasErrors() {
+        return errors > 0;
+    }
+
     public void lexicalError(int row, int col, String lexeme) {
         System.err.println("ERROR row " + row + " column " + col + ": Caracter inesperado: " + lexeme);
+        errors++;
     }
 
     public void syntaxError(Token token) {
@@ -17,5 +23,6 @@ public class ErrorHandler {
                     + ": Unexpected element";
         }
         System.err.println(msg);
+        errors++;
     }
 }
